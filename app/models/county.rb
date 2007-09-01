a6bbcab2df78d005273ccf09e04557fadf3dc3e8
walker <walker>
2007-09-01 23:49:43 +0000

    new real model for county

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,2 +1,9 @@
 class County < ActiveRecord::Base
+  attr_accessor :name
+  attr_accessor :locations
+  
+  def initialize(name)
+    @name = name
+    @locations = Location.find(:all, :conditions => ["County = ?", @name])
+  end
 end
