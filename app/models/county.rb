93b53b755f61cee5241389b0b9aa38e4d6d1d277
walker <walker>
2007-09-02 08:57:25 +0000

    add helpers for retrieving related objects

--- app/models/county.rb
+++ app/models/county.rb
@@ -6,4 +6,12 @@ class County < ActiveRecord::Base
     @name = name
     @locations = Location.find(:all, :conditions => ["County = ?", @name])
   end
+  
+  def find_all_species
+    @species = Species.find_by_sql("SELECT DISTINCT species.* FROM species, sightings, locations WHERE species.id=sightings.species_id AND sightings.location_id=locations.id AND locations.county='" + self.name + "' ORDER BY species.id")
+  end
+
+  def find_all_photos
+    Sighting.find_by_sql("SELECT sightings.* FROM sightings, locations WHERE sightings.photo='1' AND sightings.location_id=locations.id AND locations.county='" + self.name + "' ORDER BY sightings.date DESC LIMIT 50")
+  end
 end
