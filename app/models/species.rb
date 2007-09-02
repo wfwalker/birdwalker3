33cc7ad9564b7676a370a6366741698b05a238b7
walker <walker>
2007-09-02 17:23:51 +0000

    validation!!

--- app/models/species.rb
+++ app/models/species.rb
@@ -3,6 +3,8 @@ class Species < ActiveRecord::Base
   has_many :trips, :through => :sightings, :select => "DISTINCT trips.*", :order => "trips.ignored DESC"
   has_many :locations, :through => :sightings, :select => "DISTINCT locations.*", :order => "locations.state, locations.county, locations.name"
   
+  validates_presence_of :commonname, :latinname, :abbreviation
+  
   # TODO: some cleaner way to do this!
   def Species.find_all_seen
     Species.find_by_sql "SELECT DISTINCT(species.id), species.* from species, sightings WHERE species.id=sightings.species_id ORDER BY species.id"
