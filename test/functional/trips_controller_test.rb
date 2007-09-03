18c55429765e2b4000949515bd895599d29e0508
walker <walker>
2007-09-03 22:28:23 +0000

    finish scaffold Trip

--- test/functional/trips_controller_test.rb
+++ test/functional/trips_controller_test.rb
@@ -5,14 +5,88 @@ require 'trips_controller'
 class TripsController; def rescue_action(e) raise e end; end
 
 class TripsControllerTest < Test::Unit::TestCase
+  fixtures :trips
+
   def setup
     @controller = TripsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+
+    @first_id = trips(:first).id
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_index
+    get :index
+    assert_response :success
+    assert_template 'list'
+  end
+
+  def test_list
+    get :list
+
+    assert_response :success
+    assert_template 'list'
+
+    assert_not_nil assigns(:trips)
+  end
+
+  def test_show
+    get :show, :id => @first_id
+
+    assert_response :success
+    assert_template 'show'
+
+    assert_not_nil assigns(:trip)
+    assert assigns(:trip).valid?
+  end
+
+  def test_new
+    get :new
+
+    assert_response :success
+    assert_template 'new'
+
+    assert_not_nil assigns(:trip)
+  end
+
+  def test_create
+    num_trips = Trip.count
+
+    post :create, :trip => {}
+
+    assert_response :redirect
+    assert_redirected_to :action => 'list'
+
+    assert_equal num_trips + 1, Trip.count
+  end
+
+  def test_edit
+    get :edit, :id => @first_id
+
+    assert_response :success
+    assert_template 'edit'
+
+    assert_not_nil assigns(:trip)
+    assert assigns(:trip).valid?
+  end
+
+  def test_update
+    post :update, :id => @first_id
+    assert_response :redirect
+    assert_redirected_to :action => 'show', :id => @first_id
+  end
+
+  def test_destroy
+    assert_nothing_raised {
+      Trip.find(@first_id)
+    }
+
+    post :destroy, :id => @first_id
+    assert_response :redirect
+    assert_redirected_to :action => 'list'
+
+    assert_raise(ActiveRecord::RecordNotFound) {
+      Trip.find(@first_id)
+    }
   end
 end
