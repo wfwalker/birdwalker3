c1e935d9c723b85b8d568e022bbd7e71a764ecd6
walker <walker>
2007-09-03 15:45:17 +0000

    set predicates for menu coloring

--- app/controllers/bird_walker_controller.rb
+++ app/controllers/bird_walker_controller.rb
@@ -1,3 +1,15 @@
 class BirdWalkerController < ApplicationController
   layout "standard"
+  
+  def isLocation
+    false
+  end
+  
+  def isTrip
+    false
+  end
+  
+  def isSpecies
+    false
+  end
 end
