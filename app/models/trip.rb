877e4a110d32105dfde5bd2f4d7a3056658a3d39
walker <walker>
2007-09-03 22:27:55 +0000

    validate trip name

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,6 +3,8 @@ class Trip < ActiveRecord::Base
   has_many :species, :through => :sightings, :select => "DISTINCT species.*", :order => "species.id"
   has_many :locations, :through => :sightings, :select => "DISTINCT locations.*", :order => "locations.state, locations.county, locations.name"
   
+  validates_presence_of :name
+  
   def find_all_photos
     Sighting.find(:all, :conditions => ["trip_id = " + self.id.to_s + " AND photo = 1"], :order => "date DESC")
   end
