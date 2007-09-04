9e22b70b422dfae46a9afae9606f5b183baa4375
walker <walker>
2007-09-04 06:16:52 +0000

    add find-all-trips for the county

--- app/models/county.rb
+++ app/models/county.rb
@@ -11,6 +11,10 @@ class County < ActiveRecord::Base
     @species = Species.find_by_sql("SELECT DISTINCT species.* FROM species, sightings, locations WHERE species.id=sightings.species_id AND sightings.location_id=locations.id AND locations.county='" + self.name + "' ORDER BY species.id")
   end
 
+  def find_all_trips
+    Trip.find_by_sql("SELECT DISTINCT trips.* FROM trips, sightings, locations WHERE trips.id=sightings.trip_id AND sightings.location_id=locations.id AND locations.county='" + self.name + "'ORDER BY trips.ignored DESC")
+  end
+
   def find_all_photos
     Sighting.find_by_sql("SELECT sightings.* FROM sightings, locations WHERE sightings.photo='1' AND sightings.location_id=locations.id AND locations.county='" + self.name + "' ORDER BY sightings.date DESC LIMIT 50")
   end
