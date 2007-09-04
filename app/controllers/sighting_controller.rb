d7af3f11c3596dc3ca7ce9f72f774c18bf857a3d
walker <walker>
2007-09-04 16:02:56 +0000

    scaffolding for sighing

--- app/controllers/sighting_controller.rb
+++ app/controllers/sighting_controller.rb
@@ -1,7 +1,6 @@
 class SightingController < ApplicationController
-  scaffold :sighting
   layout "standard"
-  
+
   def isLocation
     false
   end
@@ -14,15 +13,57 @@ class SightingController < ApplicationController
     false
   end
   
+  def index
+    list
+    render :action => 'list'
+  end
+
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
+  def list
+    @sighting_pages, @sightings = paginate :sightings, :per_page => 10
+  end
+
+  def show
+    @sighting = Sighting.find(params[:id])
+    @page_title = @sighting.species.commonname
+  end
+
+  def new
+    @sighting = Sighting.new
+  end
+
+  def create
+    @sighting = Sighting.new(params[:sighting])
+    if @sighting.save
+      flash[:notice] = 'Sighting was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
   def edit
-    @sighting = Sighting.find(@params["id"])
+    @sighting = Sighting.find(params[:id])
     @locations = Location.find_all
     @species = Species.find_all
     @page_title = @sighting.species.commonname
   end
-  
-  def show
-    @sighting = Sighting.find(@params["id"])
-    @page_title = @sighting.species.commonname
+
+  def update
+    @sighting = Sighting.find(params[:id])
+    if @sighting.update_attributes(params[:sighting])
+      flash[:notice] = 'Sighting was successfully updated.'
+      redirect_to :action => 'show', :id => @sighting
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    Sighting.find(params[:id]).destroy
+    redirect_to :action => 'list'
   end
 end
