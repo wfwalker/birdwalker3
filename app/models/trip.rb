c1ab4106e2537747633639a65582b10c5ef0aca4
walker <walker>
2007-09-11 14:57:20 +0000

    add helper for building Hash of year => array of trips for that year

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,4 +8,17 @@ class Trip < ActiveRecord::Base
   def find_all_photos
     Sighting.find(:all, :conditions => ["trip_id = " + self.id.to_s + " AND photo = 1"], :order => "date DESC")
   end
+  
+  def Trip.map_by_year(tripList)
+    map = Hash.new
+    tripList.each do | trip |
+        if map.key? trip.ignored.year
+          map[trip.ignored.year] << trip
+        else
+          map[trip.ignored.year] = Array.new
+          map[trip.ignored.year] << trip
+        end
+    end
+    return map
+  end
 end
