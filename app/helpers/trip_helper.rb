f5725e49d1461ced0f37d75c2ac010896f1f5fd9
walker <walker>
2007-10-09 15:18:22 +0000

    oops wrong name

--- app/helpers/trip_helper.rb
+++
@@ -1,2 +0,0 @@
-module TripHelper
-end
---
+++ app/helpers/trips_helper.rb
@@ -0,0 +1,16 @@
+module TripsHelper
+  
+  def has_only_one_trip?(tripcontainer, &block)
+    yield if tripcontainer.trips.size == 1
+  end
+  
+  def show_trip_list(tripcontainer)
+    return unless tripcontainer.trips.size > 1
+    
+	  if tripcontainer.trips.size > 30
+			render :partial => 'trips/condensed_list', :object => tripcontainer
+		else
+		  render :partial => 'trips/full_list', :object => tripcontainer
+		end
+	end
+end
