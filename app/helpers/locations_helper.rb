85f4e4bf236d6dd32c5b58963fe1851fcd24be41
walker <walker>
2007-10-26 16:48:59 +0000

    add families table safe_county_name

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -3,6 +3,14 @@ module LocationsHelper
     yield if locationcontainer.locations.size == 1
   end
   
+  def safe_county_name(location)
+	  if (location.county && location.county.size > 0) then
+		  location.county
+	  else
+			"Unknown"
+	  end
+  end
+  
   def show_location_list(locationcontainer)
     return unless locationcontainer.locations.size > 1
     
