c72820b42e26e06170cbe252ad8c6e36e56f3c64
walker <walker>
2007-10-29 16:34:53 +0000

    i can has photo?

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -2,6 +2,10 @@ module TripsHelper
   def has_only_one_trip?(tripcontainer, &block)
     yield if tripcontainer.trips.size == 1
   end
+
+  def has_photo?(trip, &block)
+    yield if trip.find_all_photos.size > 1
+  end
     
   def show_trip_list(tripcontainer)
     return unless tripcontainer.trips.size > 1
