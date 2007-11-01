b27b7e73ea7bca6cf89fca3c64bbce8741f54835
walker <walker>
2007-11-01 06:15:55 +0000

    take out unused stuff

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -7,18 +7,6 @@ module LocationsHelper
     yield if locationcontainer.locations.size > 1
   end
 
-  def has_county?(location, &block)
-    yield if location.county && location.county.size > 1
-  end
-  
-  def safe_county_name(location)
-	  if (location.county && location.county.size > 0) then
-		  location.county
-	  else
-			"Unknown"
-	  end
-  end
-  
   def show_location_list(locationcontainer)
     return unless locationcontainer.locations.size > 1
     
