481ab1d65a7c7f8d1850692f288442a89957c13f
walker <walker>
2007-11-01 06:09:20 +0000

    new approach to finding photos

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,5 +1,7 @@
 class Family < ActiveRecord::Base
   has_many :species
+  has_many :sightings, :through => :species
+  has_many :sightings_with_photos, :through =>:species, :conditions => 'photo = 1'
   
   def next
     Location.find(:first, :conditions => ["id > (?)", self.id.to_s], :order => 'id')
@@ -12,14 +14,6 @@ class Family < ActiveRecord::Base
   # TODO: some cleaner way to do this!
   def Family.find_all_seen
     Family.find_by_sql "SELECT DISTINCT(families.id), families.* from families, species, sightings WHERE species.id=sightings.species_id AND species.family_id=families.id ORDER BY families.taxonomic_sort_id"
-  end  
-  
-  def find_photo
-    Sighting.find_by_sql "SELECT sightings.* FROM species, sightings WHERE sightings.photo='1' AND species.id=sightings.species_id AND species.family_id='" + self.id.to_s + "' ORDER BY sightings.id DESC LIMIT 1"
-  end  
-
-  def find_all_photos
-    Sighting.find_by_sql "SELECT sightings.* FROM species, sightings WHERE sightings.photo='1' AND species.id=sightings.species_id AND species.family_id='" + self.id.to_s + "'"
   end  
   
   def find_all_species_seen
