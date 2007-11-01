36ac95b7db4f710a4a1961eb8c6736717993445c
walker <walker>
2007-11-01 06:10:01 +0000

    real counties table makes states run really slow :-/

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,6 @@
 class State < ActiveRecord::Base
+  has_many :counties
+  has_many :locations, :through => :counties
   
   def next
     State.find(:first, :conditions => ["name > (?)", self.name.to_s], :order => 'name')
@@ -8,19 +10,21 @@ class State < ActiveRecord::Base
     State.find(:first, :conditions => ["name < (?)", self.name.to_s], :order => 'name DESC')
   end
   
-  def find_all_locations
-    @locations = Location.find(:all, :conditions => ["State = ?", self.abbreviation], :order => "locations.state, locations.county")
-  end
-
   def find_all_species
-    Species.find_by_sql("SELECT DISTINCT species.* FROM species, sightings, locations WHERE species.id=sightings.species_id AND sightings.location_id=locations.id AND locations.state='" + self.abbreviation + "' ORDER BY species.id")
+    Species.find_by_sql("SELECT DISTINCT species.* FROM species, sightings, locations, counties, states 
+      WHERE species.id=sightings.species_id AND sightings.location_id=locations.id AND
+      locations.county_id=counties.id AND counties.state_id='" + self.id.to_s + "' ORDER BY species.id")
   end
   
   def find_all_trips
-    Trip.find_by_sql("SELECT DISTINCT trips.* FROM trips, sightings, locations WHERE trips.id=sightings.trip_id AND sightings.location_id=locations.id AND locations.state='" + self.abbreviation + "' ORDER BY trips.date DESC")
+    Trip.find_by_sql("SELECT DISTINCT trips.* FROM trips, sightings, locations, counties, states 
+      WHERE trips.id=sightings.trip_id AND sightings.location_id=locations.id AND
+      locations.county_id=counties.id AND counties.state_id='" + self.id.to_s + "' ORDER BY trips.id")
   end
   
   def find_all_photos
-    Sighting.find_by_sql("SELECT sightings.* FROM sightings, locations WHERE sightings.photo='1' AND sightings.location_id=locations.id AND locations.state='" + self.abbreviation + "' ORDER BY sightings.trip_id DESC LIMIT 50")
+    Sighting.find_by_sql("SELECT DISTINCT sightings.* FROM sightings, locations, counties, states 
+      WHERE sightings.photo='1' AND sightings.location_id=locations.id AND
+      locations.county_id=counties.id AND counties.state_id='" + self.id.to_s + "' ORDER BY sightings.id")
   end
 end
