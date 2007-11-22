ad5291cdc9a58611baf68734401d60d22a37ebaf
walker <walker>
2007-11-22 07:49:05 +0000

    rename finders to use typical naming conventions

--- app/models/state.rb
+++ app/models/state.rb
@@ -10,20 +10,20 @@ class State < ActiveRecord::Base
     State.find(:first, :conditions => ["name < (?)", self.name.to_s], :order => 'name DESC')
   end
   
-  def find_all_species
-    Species.find_by_sql("SELECT DISTINCT species.* FROM species, sightings, locations, counties, states 
+  def species
+    Species.find_by_sql("SELECT DISTINCT species.* FROM species, sightings, locations, counties 
       WHERE species.id=sightings.species_id AND sightings.location_id=locations.id AND
-      locations.county_id=counties.id AND counties.state_id='" + self.id.to_s + "' ORDER BY species.id")
+      locations.county_id=counties.id AND counties.state_id='" + self.id.to_s + "'")
   end
   
-  def find_all_trips
-    Trip.find_by_sql("SELECT DISTINCT trips.* FROM trips, sightings, locations, counties, states 
+  def trips
+    Trip.find_by_sql("SELECT DISTINCT trips.* FROM trips, sightings, locations, counties 
       WHERE trips.id=sightings.trip_id AND sightings.location_id=locations.id AND
-      locations.county_id=counties.id AND counties.state_id='" + self.id.to_s + "' ORDER BY trips.id")
+      locations.county_id=counties.id AND counties.state_id='" + self.id.to_s + "'")
   end
   
-  def find_all_photos
-    Sighting.find_by_sql("SELECT DISTINCT sightings.* FROM sightings, locations, counties, states 
+  def sightings_with_photos
+    Sighting.find_by_sql("SELECT DISTINCT sightings.* FROM sightings, locations, counties 
       WHERE sightings.photo='1' AND sightings.location_id=locations.id AND
       locations.county_id=counties.id AND counties.state_id='" + self.id.to_s + "' ORDER BY sightings.id")
   end
