13a5ac7f038327354106030271a905b8fba02c02
walker <walker>
2007-11-22 07:51:28 +0000

    sort the undivided species list properly

--- app/helpers/species_helper.rb
+++ app/helpers/species_helper.rb
@@ -1,9 +1,11 @@
 module SpeciesHelper
   def show_species_list(species_list)
+    
 	  if species_list.size > 40
 			render :partial => 'species/divided_list', :object => species_list
 		else
-		  render :partial => 'species/undivided_list', :object => species_list
+      sorted_species_list = species_list.sort_by { |s| s.family.taxonomic_sort_id * 100000000000 + s.id }
+		  render :partial => 'species/undivided_list', :object => sorted_species_list
 		end
 	end
 end
