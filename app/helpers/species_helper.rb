b64950b779e0fee66e75b8ab05e6566fd13810a2
walker <walker>
2007-12-29 20:02:04 +0000

    use sorting helper in model class

--- app/helpers/species_helper.rb
+++ app/helpers/species_helper.rb
@@ -1,10 +1,10 @@
 module SpeciesHelper
   def show_species_list(species_list)
+    sorted_species_list = Species.sort_taxonomic(species_list)
     
 	  if species_list.size > 40
-			render :partial => 'species/divided_list', :object => species_list
+			render :partial => 'species/divided_list', :object => sorted_species_list
 		else
-      sorted_species_list = species_list.sort_by { |s| s.family.taxonomic_sort_id * 100000000000 + s.id }
 		  render :partial => 'species/undivided_list', :object => sorted_species_list
 		end
 	end
