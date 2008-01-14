5dd9aaa4a3afb31184c9070c8b4f163051b3966a
walker <walker>
2008-01-14 07:15:09 +0000

    don't show so many summaries

--- app/controllers/bird_walker_controller.rb
+++ app/controllers/bird_walker_controller.rb
@@ -2,7 +2,7 @@ class BirdWalkerController < ApplicationController
   helper :trips
   
   def index
-    @recent_trips = Trip.find(:all, :limit => 20, :order => 'date DESC')
+    @recent_trips = Trip.find(:all, :limit => 10, :order => 'date DESC')
   end
 
   def about
