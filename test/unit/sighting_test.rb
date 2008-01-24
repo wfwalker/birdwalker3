ba53219d4348e8069d6877f1c1c59758b5286825
walker <walker>
2008-01-24 07:28:57 +0000

    i think they're supposed to be singular

--- test/unit/sighting_test.rb
+++ test/unit/sighting_test.rb
@@ -3,8 +3,24 @@ require File.dirname(__FILE__) + '/../test_helper'
 class SightingTest < Test::Unit::TestCase
   fixtures :sightings
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_associations
+    @location = Location.create(:name => 'bird park', :count => 'bird county')
+    @species = Species.create
+    @species.save
+    @trip = Trip.create
+    @trip.save
+
+    @sighting = Sighting.create    
+    @sighting.trip_id = @trip.id
+    @sighting.species_id = @species.id
+    @sighting.location_id = @location.id
+    @sighting.save
+    @sighting.reloadload
+    
+    @location.reload
+    @species.reload
+    @trip.reload
+    
+    assert_equal 1, @location.sightings.size
   end
 end
