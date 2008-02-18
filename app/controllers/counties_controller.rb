1b00d1b865681442671698be607eb9682b1f38f2
walker <walker>
2008-02-18 09:10:39 +0000

    species by year view (refactor)_

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -14,25 +14,7 @@ class CountiesController < ApplicationController
   def show_species_by_year
     @county = County.find(params["id"])
     
-    @map = {}
-    @totals = (1996.2008).to_a
-    for year in (1996..2008) do
-      @totals[year] = 0
-    end
-		
-    
-    for sighting in @county.sightings do
-        if (sighting.species) then
-          if (!@map[sighting.species]) then
-            @map[sighting.species] = (1996..2008).to_a
-          end
-          
-          if (@map[sighting.species][sighting.trip.date.year] != 'X') then
-            @map[sighting.species][sighting.trip.date.year] = 'X'
-            @totals[sighting.trip.date.year] = @totals[sighting.trip.date.year] + 1
-          end
-        end
-    end
+    @map, @totals = Sighting.map_by_year_and_species(@county.sightings)
     
     @county.previous && @previous_url = county_url(@county.previous)
     @county.next && @next_url = county_url(@county.next)
