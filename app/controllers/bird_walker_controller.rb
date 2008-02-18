19428a93cd03ad68ffde6d9794e8855a5279a186
walker <walker>
2008-02-18 09:11:02 +0000

    year to date thing

--- app/controllers/bird_walker_controller.rb
+++ app/controllers/bird_walker_controller.rb
@@ -5,6 +5,8 @@ class BirdWalkerController < ApplicationController
     @recent_trips = Trip.find(:all, :limit => 10, :order => 'date DESC')
     @bird_of_the_week = Species.bird_of_the_week
     @photo_of_the_week = @bird_of_the_week.photo_of_the_week
+    @this_year_species = Species.year_to_date(Date.today.year)
+    @last_year_species = Species.year_to_date(Date.today.year - 1)
   end
 
   def about
