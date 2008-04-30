dd0b4916c30f640a49f32d0eefb220bcf758cc4d
walker <walker>
2008-04-30 15:53:12 +0000

    database dump

---
+++ dump-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#EXECUTABLE="/usr/local/mysql-standard-5.0.27-osx10.4-i686/bin/mysqldump"
+#EXECUTABLE="/usr/local/mysql-5.0.45-osx10.4-i686/bin/mysqldump"
+EXECUTABLE=/usr/local/mysql/bin/mysqldump
+
+AUTHFLAGS="-u root HelloWorld_development"
+OTHERFLAGS="--extended-insert=FALSE --quote-names=TRUE --complete-insert=TRUE --comments=FALSE --add-locks=FALSE"
+
+$EXECUTABLE $OTHERFLAGS $AUTHFLAGS > test.sql
