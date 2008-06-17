8a159d2edd018b0718061691a34eaad827bb0fc8
walker <walker>
2008-06-17 16:27:17 +0000

    sensible county lists

--- app/helpers/counties_helper.rb
+++ app/helpers/counties_helper.rb
@@ -1,2 +1,9 @@
 module CountiesHelper
+  def show_county_list(county_list, headings_flag)
+	  if county_list.size > 30
+			render :partial => 'counties/divided_list', :object => county_list, :locals => { :show_headings => headings_flag }
+		else
+		  render :partial => 'counties/undivided_list', :object => county_list, :locals => { :show_headings => headings_flag }
+		end
+	end
 end
