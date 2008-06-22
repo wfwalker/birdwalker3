4fe779e89f1722f9d93ffa923d7a83144ee6f0c5
walker <walker>
2008-06-22 07:39:30 +0000

    new routes for photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :counties
   map.resources :sightings
+  map.resources :photos
   map.resources :trips
   map.resources :states
   map.resources :locations
