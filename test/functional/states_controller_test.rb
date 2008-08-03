8adf84ea99608cf9a730bbf74e28bd2fdbc6e5d8
walker <walker>
2008-08-03 03:53:31 +0000

    a few tests for state controller

--- test/functional/states_controller_test.rb
+++ test/functional/states_controller_test.rb
@@ -5,14 +5,38 @@ require 'states_controller'
 class StatesController; def rescue_action(e) raise e end; end
 
 class StatesControllerTest < Test::Unit::TestCase
+  fixtures :counties, :states
+
   def setup
     @controller = StatesController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    
+    @first_id = states(:state_one).id
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_index
+    get :index
+    assert_response :success
+    assert_template 'list'
+  end
+
+  def test_list
+    get :list
+
+    assert_response :success
+    assert_template 'list'
+
+    assert_not_nil assigns(:states)
+  end  
+  
+  def test_show
+    get :show, :id => @first_id
+
+    assert_response :success
+    assert_template 'show'
+
+    assert_not_nil assigns(:state)
+    assert assigns(:state).valid?
   end
 end
