74144c27b50735b7df49f7fe3591ab4216eda0b3
walker <walker>
2008-08-12 07:12:02 +0000

    fix typo with map_by_month

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -74,7 +74,7 @@ class Sighting < ActiveRecord::Base
   end                   
 
   def Sighting.map_by_month(sighting_list)
-    totals = (1996.2008).to_a
+    totals = (1..12).to_a
     for month in (1..12) do
       totals[month] = 0
     end
