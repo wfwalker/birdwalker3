1684430127721c2a4a75e5d135cdd8e4355cb1c1
walker <walker>
2008-12-02 07:23:58 +0000

    oops, take out dumb pointless test

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -13,8 +13,4 @@ class ApplicationHelperTest < HelperTestCase
   def test_always_passes
     # do nothing - required by test/unit
   end
-  
-  def test_timeout
-    session[:username] = 'pants'
-  end
 end
