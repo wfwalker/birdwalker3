675786850cd5f2897789330f57bb4b09f9b3a4d5
walker <walker>
2009-02-08 17:17:21 +0000

    upgrade to rails 2.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,12 +9,17 @@ config.cache_classes = false
 config.whiny_nils = true
 
 # Enable the breakpoint server that script/breakpointer connects to
-config.breakpoint_server = true
+
+# WFW commented out during rails upgrade
+# config.breakpoint_server = true
 
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
-config.action_view.cache_template_extensions         = false
+
+# WFW commented out during rails upgrade
+#config.action_view.cache_template_extensions         = false
+
 config.action_view.debug_rjs                         = true
 
 # Don't care if the mailer can't send
