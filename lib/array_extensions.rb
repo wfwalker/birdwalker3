76a9698c32006b36eea1ee65895b6543103347a8
William Walker <wilwalke@WILWALKE-MBP.local>
2009-03-01 23:38:29 -0800

    add rdoc comment

--- lib/array_extensions.rb
+++ lib/array_extensions.rb
@@ -1,6 +1,8 @@
 # Grant Goodale's array extensions
 # Usage: [1,2,3,4,5,6] / 2 #=> [[1,2],[3,4],[5,6]]
 class Array
+  
+ # Divides one array into subarrays of the specified length
  def /(len)
        a = []
        each_with_index do |x,i|
