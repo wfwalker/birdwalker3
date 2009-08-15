754db7a8681d1369be500697c2972e64ba2cfe18
William Walker <wilwalke@WILWALKE-MBP-2.local>
2009-08-15 12:32:34 -0700

    add basic tests for related objects

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -1,10 +1,33 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class LocationTest < Test::Unit::TestCase
-  fixtures :locations
+  fixtures :photos, :locations, :species, :trips, :sightings, :counties, :states
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def setup
+    @first_id = locations(:location_one).id
+  end
+
+  def test_abbreviation
+    aLocation = Location.find(@first_id)
+    
+    # abbreviation for "First Place" is "FP"
+    assert_equal aLocation.name, "First Place"
+    assert_equal aLocation.abbreviation, "FP"
+  end
+  
+  def test_sightings
+    assert_equal 2, Location.find(@first_id).sightings.size, "test fixture has two sightings for this location"
+  end
+
+  def test_species
+    assert_equal 1, Location.find(@first_id).species.size, "test fixture has one species for this location"
+  end
+
+  def test_trips
+    assert_equal 2, Location.find(@first_id).trips.size, "test fixture has two trips for this location"
+  end
+
+  def test_photos
+    assert_equal 1, Location.find(@first_id).photos.size, "test fixture has one photos for this location"
   end
 end
--- test/unit/species_test.rb
+++ test/unit/species_test.rb
@@ -1,18 +1,44 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require 'species'
 
 class SpeciesTest < Test::Unit::TestCase
-  fixtures :species, :families, :photos
+  fixtures :species, :families, :photos, :locations, :trips, :sightings
+
+  def setup
+    @first_id = species(:species_one).id
+  end
 
   # Replace this with your real tests.
   def test_fixtures
     assert Species.count == 2
-  end                 
+  end           
+  
+  def test_common
+    assert_equal false, Species.find(@first_id).common?, "sample test species should not be common"
+  end      
   
   def test_find_all_countable_photographed
-    assert Species.countable.photographed.size == 2
+    assert_equal 2, Species.countable.photographed.size, "test fixture has two countable, photographed species"
   end
 
   def test_find_all_photographed
-    assert Species.photographed.size == 2
+    assert_equal 2, Species.photographed.size, "test fixture has two photographed species"
   end
+  
+  def test_sightings
+    assert_equal 2, Species.find(@first_id).sightings.size, "test fixture has two sightings for this species"
+  end
+
+  def test_locations
+    assert_equal 1, Species.find(@first_id).locations.size, "test fixture has one location for this species"
+  end
+
+  def test_trips
+    assert_equal 2, Species.find(@first_id).trips.size, "test fixture has two trips for this species"
+  end
+
+  def test_photos
+    assert_equal 1, Species.find(@first_id).photos.size, "test fixture has one photos for this species"
+  end
+  
 end
