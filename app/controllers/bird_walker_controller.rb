3d3b47b7242aa9065ad4d4e399c8c35a7a4b9d39
William Walker <wilwalke@WILWALKE-MBP-2.local>
2009-08-16 11:58:25 -0700

    stop using photo_of_the_week since bird_of_the_week is always photographed

--- app/controllers/bird_walker_controller.rb
+++ app/controllers/bird_walker_controller.rb
@@ -24,9 +24,6 @@ class BirdWalkerController < ApplicationController
     @most_visited_locations = Location.most_visited_recently
     
     @bird_of_the_week = Species.bird_of_the_week
-    if @bird_of_the_week != nil then
-      @photo_of_the_week = @bird_of_the_week.photo_of_the_week
-    end
 
     @this_year_species = Species.year_to_date(Date.today.year)
     @last_year_species = Species.year_to_date(Date.today.year - 1)
@@ -41,9 +38,6 @@ class BirdWalkerController < ApplicationController
     @recent_trips = Trip.find(:all, :limit => 10, :order => 'date DESC')     
     
     @bird_of_the_week = Species.bird_of_the_week 
-    if (@bird_of_the_week != nil)
-      @photo_of_the_week = @bird_of_the_week.photo_of_the_week
-    end
 
     @this_year_species = Species.year_to_date(Date.today.year)
     @last_year_species = Species.year_to_date(Date.today.year - 1)  
