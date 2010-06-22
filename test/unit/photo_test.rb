b7f8ced3f9b0065ecfa25b1094137cd68774729e
William Walker <walker@vermillion.local>
2010-06-22 08:57:52 -0700

    change test code to reflect the fact that we no longer use tiny_thumb

--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -8,11 +8,11 @@ class PhotoTest < Test::Unit::TestCase
     @first_id = photos(:photo_one).id
   end
 
-  def test_tiny_thumb_hostname
+  def test_thumb_hostname
     aPhoto = Photo.find(@first_id)
-    assert_no_match /localhost/, aPhoto.tiny_thumb(), "tiny_thumb URL with no argument should NOT contain hostname (i. e., localhost)"
-    assert_no_match /http/, aPhoto.tiny_thumb(), "tiny_thumb URL with no argument should NOT 'http', since it should just be relative"
+    assert_no_match /localhost/, aPhoto.thumb(), "thumb URL with no argument should NOT contain hostname (i. e., localhost)"
+    assert_no_match /http/, aPhoto.thumb(), "thumb URL with no argument should NOT 'http', since it should just be relative"
     
-    assert_match /localhost/, aPhoto.tiny_thumb('localhost'), "tiny_thumb URL should contain hostname (i. e., localhost)"
+    assert_match /localhost/, aPhoto.thumb('localhost'), "thumb URL should contain hostname (i. e., localhost)"
   end
 end
