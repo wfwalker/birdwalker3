d7ed92bc57acf796b4e3afb6a34dabb8e72ae65d
William Walker <wilwalke@WILWALKE-MBP.local>
2011-03-19 23:34:21 -0700

    look for location name as command-line arg find all sightings within 5 miles of that place within the last 5 days

--- ebirdAPI.rb
+++ ebirdAPI.rb
@@ -22,5 +22,28 @@ def get_XML_from_eBird(in_path, in_args)
   Document.new(xmlData.body())
 end
 
-ebirdXmlData = get_XML_from_eBird('data/obs/geo/recent', {'lng' => '-76.51', 'lat' => '42.46'})
-ebirdXmlData.write($stdout, 3)
+def parse_XML_as_ebird_sightings(in_XML)
+  entries = []
+  XPath.each(in_XML, "/response/result/sighting") { | sighting_xml |
+    entry = {}
+    entry['loc-name'] = sighting_xml.get_elements("loc-name")[0].text()
+    entry['common-name'] = sighting_xml.get_elements("com-name")[0].text()
+    entry['date'] = Date.strptime(sighting_xml.get_elements("obs-dt")[0].text())
+    entries << entry
+  }
+  entries
+end
+
+location_id = $*
+
+the_location = Location.find(location_id)[0]
+
+puts the_location.name
+
+ebirdXmlData = get_XML_from_eBird('data/obs/geo/recent', {'lng' => the_location.longitude, 'lat' => the_location.latitude, 'dist' => 5, 'back' => 5})
+sightings = parse_XML_as_ebird_sightings(ebirdXmlData)
+
+sightings.each { | a_sighting |
+  puts "%s on %s at %s\n" % [a_sighting["common-name"], a_sighting["date"], a_sighting["loc-name"]]
+}
+
