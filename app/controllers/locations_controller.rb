0982e3970c4444dfae2aea6771836ee086cacb3f
Bill Walker <bwalker@mozilla.com>
2012-10-30 20:52:30 -0700

    don't cache locations list / index
    don't cache locations list / index because the cacheing breaks the
    respond_to tag! see issue 7

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -15,7 +15,7 @@ class LocationsController < ApplicationController
   before_filter :verify_credentials, :only => [:new, :create, :edit, :update, :destroy]  
   before_filter :update_activity_timer, :except => [:new, :create, :edit, :update, :destroy]  
   
-  caches_action :list, :index, :show, :layout => false
+  caches_action :show, :layout => false
   
   def page_kind
     "locations"
