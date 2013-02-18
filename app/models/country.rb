09f7821fb26f2ce60af38ee1c916f113aa041c24
Bill Walker <bwalker@mozilla.com>
2013-02-18 10:58:31 -0800

    relations for countries
    wire up relations for countries to have sightings, locations, species,
    trips

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,24 @@
 class Country < ActiveRecord::Base
   attr_accessible :name, :reference_url
+
+  has_many :states
+
+  has_many :sightings, :through => :states do
+      def life        
+        Sighting.first_per_species(self)
+      end
+  end
+
+  has_many :species, :through => :sightings, :uniq => true, :order => 'species.id'
+      
+  has_many :trips, :through => :sightings, :uniq => true
+  
+  has_many :photos, :through => :locations, :uniq => true
+
+  has_many :locations, :through => :states do
+    def with_lat_long
+      Location.with_lat_long(self)
+    end
+  end 
+
 end
