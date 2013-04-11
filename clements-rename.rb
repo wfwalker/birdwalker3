07e20cf70853e20901d744f0c51447eb67c8bc16
Bill Walker <bwalker@mozilla.com>
2013-04-10 23:46:55 -0700

    fixing up renamer script
    added health_report function
    go ahead with renames even if common_name does not match
    only show UNUSED RENAMES if there are some

--- clements-rename.rb
+++ clements-rename.rb
@@ -5,6 +5,13 @@ require File.expand_path('../config/boot', __FILE__)
 # need CSV package to read data files
 require 'csv'
 
+def health_report
+  taxon_count = Taxon.find(:all).count
+  species_seen_count = Species.seen.count
+  sightings_missing_taxon_count = Sighting.find_by_sql("SELECT * FROM sightings where sightings.taxon_latin_name IS NULL").count
+  puts "taxons %d species seen %d sightings lacking taxon %d" % [taxon_count, species_seen_count, sightings_missing_taxon_count]
+end
+
 def read_renames
   renames_text = File.read('./Shipman-Clements-renames.csv')
   renames = CSV.parse(renames_text, :headers => true)
@@ -81,46 +88,61 @@ def apply_renames(renameDictionary)
     # there's no rename but there is a taxon that matches the species
     elsif Taxon.find_by_latin_name(species.latin_name)
       taxon = Taxon.find_by_latin_name(species.latin_name)
+
       # both latin and common names match, good to go
-      if taxon.common_name.downcase == species.common_name.downcase and taxon.latin_name.downcase == species.latin_name.downcase then
-        # puts "MATCH %s %s == %s %s" % [species.latin_name, species.common_name, clementsMatch["SCIENTIFIC NAME"], clementsMatch["ENGLISH NAME"]]
-        # add the new latin_name to all affected sightings
-        Sighting.update_all({:taxon_latin_name => taxon.latin_name}, {:species_id => species.id})
-        # add the new latin_name to all affected photos 
-        Photo.update_all({:taxon_latin_name => taxon.latin_name}, {:species_id => species.id})
-        # copy over the corresponding abbreviation to the new taxon
-        Taxon.update_all({:abbreviation => species.abbreviation}, {:latin_name => taxon.latin_name})
+      if taxon.common_name.downcase != species.common_name.downcase then
+        # there is a taxon for this species latin name, but the common names don't match
+        puts "NOTE common name upgrade %s != %s" % [species.common_name, taxon.common_name]
+        brokenMatchCount += 1
+      else
         # increment count of successful matches
         matchCount += 1
-      else
-        # there is a taxon for this species latin name, but the common names don't match
-        puts "BROKEN (%d) %s %s == %s %s" % [species.sightings.count, species.latin_name, species.common_name, taxon.latin_name, taxon.common_name]
-        brokenMatchCount += 1
       end
+
+      # add the new latin_name to all affected sightings
+      Sighting.update_all({:taxon_latin_name => taxon.latin_name}, {:species_id => species.id})
+      # add the new latin_name to all affected photos 
+      Photo.update_all({:taxon_latin_name => taxon.latin_name}, {:species_id => species.id})
+      # copy over the corresponding abbreviation to the new taxon
+      Taxon.update_all({:abbreviation => species.abbreviation}, {:latin_name => taxon.latin_name})
+
     # there's no rename for this species, nor a taxon that matches by latin name
     else
       puts "FAIL (%d) %s %s" % [species.sightings.count, species.common_name, species.latin_name]
+      puts "adding note to %d trips that have this FAIL" % species.trips.count
+      species.trips.each do | a_trip |
+        revised_notes = a_trip.notes + (" Also saw %s" % species.common_name)
+        a_trip.notes = revised_notes
+        a_trip.save()
+      end
+      species.sightings.destroy_all()
       failCount += 1
     end
   end
 
   puts "match %d broken %d rename go %d notdone %d fail %d" % [matchCount, brokenMatchCount, renameGoCount, renameNotDoneCount, failCount]
 
-  puts "UNUSED RENAMES"
-  renameDictionary.each_pair do |k, v|
-    if v != 'REMOVED'
-      puts k, v
-    end
-  end  
+  if renameDictionary.keys.count > 0 then
+    puts "UNUSED RENAMES"
+    renameDictionary.each_pair do |k, v|
+      if v != 'REMOVED'
+        puts k, v
+      end
+    end  
+  end
 end
 
 
 # -------------------------------
 puts "Clements-Shipman scientific name unification"
 
+health_report();
 renameDictionary = read_renames();
-populate_taxons_from_clements();
+health_report();
+# populate_taxons_from_clements();
+health_report();
 apply_renames(renameDictionary);
+health_report();
 
 # -------------------------------
 
