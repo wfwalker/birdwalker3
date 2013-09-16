27bf03cf36f24f5913ad4afc7942deaf3c3712c5
Bill Walker <bwalker@mozilla.com>
2013-09-16 11:00:58 -0700

    remove species table
    get rid of model, controller, views, helpers, tests, fixtures

--- test/functional/species_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-require 'species_controller'
-
-# Re-raise errors caught by the controller.
-class SpeciesController; def rescue_action(e) raise e end; end
-
-class SpeciesControllerTest < ActionController::TestCase
-end
