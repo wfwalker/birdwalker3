3816af72698c5a813da8956ff8f5b58b839fd561
Bill Walker <bwalker@mozilla.com>
2013-09-16 11:10:33 -0700

    remove families table
    remove helpers, views, controller, model, tests, fixtures

--- test/functional/families_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-require 'families_controller'
-
-# Re-raise errors caught by the controller.
-class FamiliesController; def rescue_action(e) raise e end; end
-
-class FamiliesControllerTest < ActionController::TestCase
-end
